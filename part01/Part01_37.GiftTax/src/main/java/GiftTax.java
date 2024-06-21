b5f3ff878c49ab26346324a5f6c6be3c0d0d6f91
Boolevad <123184634+Boolevad@users.noreply.github.com>
2024-06-21 16:19:19 +0300

    Add files via upload

---
+++ part01/Part01_37.GiftTax/src/main/java/GiftTax.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class GiftTax {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Value of the gift?");
+        int value = Integer.valueOf(scan.nextLine());
+        double tax = 0;
+        if (value < 5000) {
+            System.out.println("No tax!");
+        } else if (value < 25000) {
+            tax = 100 + (1.0*(value - 5000)*0.08);
+            System.out.println("Tax: " + tax);        
+        } else if (value < 55000) {
+            tax = 1700 + (1.0*(value - 25000)*0.10);
+            System.out.println("Tax: " + tax);        
+        } else if (value < 200000) {
+            tax = 4700 + (1.0*(value - 55000)*0.12);
+            System.out.println("Tax: " + tax);        
+        } else if (value < 1000000) {
+            tax = 22100 + (1.0*(value - 200000)*0.15);
+            System.out.println("Tax: " + tax);       
+        } else {
+            tax = 142100 + (1.0*(value - 1000000)*0.17);
+            System.out.println("Tax: " + tax);
+        }   
+
+    }
+}
