dcf8a5de073ddf0550e95fd01f58c112c382bba2
Boolevad <123184634+Boolevad@users.noreply.github.com>
2024-06-21 17:06:42 +0300

    Add files via upload

---
+++ part04/Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,12 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        
+        Account jimsAccount = new Account("Jim's account", 100.0);
+        jimsAccount.deposit(20.0);
+        System.out.println(jimsAccount);
+    }
+}
